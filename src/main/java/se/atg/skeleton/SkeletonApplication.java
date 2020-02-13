a09143c0f0ca6caefc4de0b66de61fbedd6e6b92
Mahesh Nayak <mahesh.nayak@atg.se>
2020-02-13 15:54:34 +0100

    files

--- src/main/java/se/atg/skeleton/SkeletonApplication.java
+++ src/main/java/se/atg/skeleton/SkeletonApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+import java.io.*;
 @SpringBootApplication
 @RefreshScope
 public class SkeletonApplication {
 
 	public static void main(String[] args) {
- ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
- DataOutputStream requestLoggerStream = new DataOutputStream(byteArrayOutputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream requestLoggerStream = new DataOutputStream(byteArrayOutputStream);
+
 		new SpringApplicationBuilder(SkeletonApplication.class)
 				.profiles("vault")
 				.run(args);
