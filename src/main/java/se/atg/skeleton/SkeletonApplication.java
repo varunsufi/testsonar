3c1a562292bacab2da81e0eedb34e7130853b41d
Mahesh Nayak <mahesh.nayak@atg.se>
2020-02-19 11:12:21 +0100

    add build test

--- src/main/java/se/atg/skeleton/SkeletonApplication.java
+++ src/main/java/se/atg/skeleton/SkeletonApplication.java
@@ -3,15 +3,18 @@ package se.atg.skeleton;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.io.*;
 @SpringBootApplication
 @RefreshScope
 public class SkeletonApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         DataOutputStream requestLoggerStream = new DataOutputStream(byteArrayOutputStream);
+        String ip = "192.168.12.42"; // Noncompliant
+          Socket socket = new Socket(ip, 6667);
 
 		new SpringApplicationBuilder(SkeletonApplication.class)
 				.profiles("vault")
